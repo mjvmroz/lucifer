bed88dbf62b18071b968e9b9846cc781726215a8
Michael Mroz <michael@mroz.io>
2021-09-11 22:07:51 -0700

    added a vec3, mostly written by derive_more and copilot

--- compute/src/lib.rs
+++ compute/src/lib.rs
@@ -1,6 +1,9 @@
 mod utils;
+mod vec3;
 
 use wasm_bindgen::prelude::*;
+
+use crate::vec3::Color;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global allocator.
 #[cfg(feature = "wee_alloc")]
@@ -18,13 +21,12 @@ pub fn get_buffer(width: u32, height: u32) -> Vec<u8> {
         .into_iter()
         .flat_map(|x| {
             (0..height).into_iter().flat_map(move |y| {
-                //
-                let r: u8 = ((x as f64 / (width - 1) as f64) * 255.999) as u8;
-                let g: u8 = ((y as f64 / (height - 1) as f64) * 255.999) as u8;
-                let b = 64u8;
-                let a = 255u8;
-
-                vec![r, g, b, a].into_iter()
+                Color::new(
+                    x as f64 / (width - 1) as f64,
+                    y as f64 / (height - 1) as f64,
+                    0.25,
+                )
+                .to_rgba()
             })
         })
         .collect()
---
+++ compute/src/vec3.rs
@@ -0,0 +1,87 @@
+use derive_more::{
+    Add, AddAssign, Constructor, Display, Div, DivAssign, Mul, MulAssign, Product, Sub, SubAssign,
+    Sum,
+};
+
+#[derive(
+    Debug,
+    Default,
+    Copy,
+    Clone,
+    PartialEq,
+    Constructor,
+    Add,
+    AddAssign,
+    Sub,
+    SubAssign,
+    Div,
+    DivAssign,
+    Mul,
+    MulAssign,
+    Product,
+    Sum,
+    Display,
+)]
+#[display(fmt = "({}, {}, {})", x, y, z)]
+pub(crate) struct Vec3 {
+    pub(crate) x: f64,
+    pub(crate) y: f64,
+    pub(crate) z: f64,
+}
+
+pub(crate) type Point3 = Vec3;
+pub(crate) type Color = Vec3;
+
+impl Vec3 {
+    pub(crate) fn zero() -> Vec3 {
+        Default::default()
+    }
+
+    pub(crate) fn one() -> Vec3 {
+        Vec3 {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0,
+        }
+    }
+
+    pub(crate) fn length_squared(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub(crate) fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub(crate) fn dot(&self, other: &Vec3) -> f64 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    pub(crate) fn cross(&self, other: &Vec3) -> Vec3 {
+        Vec3 {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+
+    pub(crate) fn unit_vector(&self) -> Vec3 {
+        *self / self.length()
+    }
+
+    pub(crate) fn to_rgba(&self) -> [u8; 4] {
+        let r = (self.x * 255.999) as u8;
+        let g = (self.y * 255.999) as u8;
+        let b = (self.z * 255.999) as u8;
+        let a = 255;
+        [r, g, b, a]
+    }
+}
+
+impl std::ops::Mul<Self> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        Vec3::new(self.x * rhs.x, self.y * rhs.y, self.z * rhs.z)
+    }
+}
